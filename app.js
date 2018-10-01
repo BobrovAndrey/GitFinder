1503bf763dbf40cfd282a57e0866beac1fd1448f
Andrey Bobrov <Bobrov.law@gmail.com>
2018-10-01 17:20:57 +0300

    Work in progress

---
+++ app.js
@@ -0,0 +1,31 @@
+//Init GitHub
+const github = new Github;
+//Init UIclass
+const ui = new UI;
+
+//Search input
+const searchUser = document.getElementById('searchUser');
+
+//Search input event listener
+searchUser.addEventListener('keyup', (e)=>{
+  //Get input text
+  const userText = e.target.value;
+
+  if(userText !==''){
+    //Make HTTP call
+    github.getUser(userText)
+    .then(data => {
+      if(data.profile.messge === 'Not Found'){
+        //Show alert
+
+      } else {
+        //Show profile
+        ui.showProfile(data.profile);
+
+      }
+    })
+  } else {
+    //Clear profile
+
+  }
+});
---
+++ github.js
@@ -0,0 +1,16 @@
+class Github{
+constructor (){
+  this.client_id = '37180df27d93f5337c19';
+  this.client_secret = '860855676c8da5a0fcef9a084ca0477122f1483b';
+}
+
+async getUser(user){
+  const profileResponce = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+  const profileData = await profileResponce.json();
+
+    return{
+      profile
+    }
+  }
+}

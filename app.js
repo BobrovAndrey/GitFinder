de5c272d4205656efba2cee70885dfd03bddfa3c
Andrey Bobrov <Bobrov.law@gmail.com>
2018-10-02 15:19:01 +0300

    Progress

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ searchUser.addEventListener('keyup', (e)=>{
   //Get input text
   const userText = e.target.value;
 
-  if(userText !==''){
+  if(userText !== ''){
     //Make HTTP call
     github.getUser(userText)
     .then(data => {
-      if(data.profile.messge === 'Not Found'){
+      if(data.profile.message === 'Not Found'){
         //Show alert
+        ui.showAlert('User not found','alert alert-danger');
 
       } else {
         //Show profile
@@ -26,6 +27,7 @@ searchUser.addEventListener('keyup', (e)=>{
     })
   } else {
     //Clear profile
+    ui.clearProfile();
 
   }
   
